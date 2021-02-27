fdcc83dfbe39895170363443f95fc39314052f2f
Wojciech Arendarczyk <wojciech.arendarczyk@student.uj.edu.pl>
2021-02-27 12:56:28 +0100

    basic connection

--- server/src/Server.java
+++ server/src/Server.java
@@ -1,2 +1,65 @@
-package PACKAGE_NAME;public class server {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
+public class Server {
+    static final int port = 2137;
+    ServerSocket servs = null;
+    Socket client = null;
+    PrintWriter ow = null;
+    BufferedReader br = null;
+    String roomId;
+
+    HashMap<String, Integer> rooms = new HashMap<>();
+
+    void startServer(){
+        while(true) {
+            try {
+                servs = new ServerSocket(port);
+            } catch (IOException e) {
+                System.out.println("Problem z utworzeniem ServerSocket");
+            }
+            try {
+                client = servs.accept();
+            } catch (IOException e) {
+                System.out.println("problem z połączeniem klienckim");
+            }
+            System.out.println("connected with client");
+            try {
+                ow = new PrintWriter(client.getOutputStream(), true);
+                br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            }
+            catch(IOException e){
+                System.out.println("Problem z  utworzeniem strumieni");
+            }
+
+            try{
+            roomId = br.readLine();
+            }
+            catch(IOException e){
+                System.out.println("Nie da się odczytać id pokoju");
+            }
+            System.out.println(roomId);
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+       Server srv = new Server();
+       srv.startServer();
+    }
 }
