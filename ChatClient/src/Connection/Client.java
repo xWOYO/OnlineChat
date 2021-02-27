b326c0a905e2cabd58a41c29c3d48793b7038f37
Maciej Mlaskawa <maciej.mlaskawa@gmail.com>
2021-02-27 17:45:03 +0100

    Updated .gitignore, implemented communication between Client and GUI, allowing user to choose room name

--- ChatClient/src/Connection/Client.java
+++ ChatClient/src/Connection/Client.java
@@ -1,26 +1,32 @@
+package Connection;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.*;
 
 
+
 public class Client {
     Socket mainServer;
+    Socket chatServer;
     PrintWriter mainServerWriter;
     BufferedReader mainServerReader;
 
-    void start() throws Exception{
+    public void initiateConnection() throws Exception{
 
         mainServer = new Socket("192.168.0.73", 2137);
         mainServerWriter = new PrintWriter(mainServer.getOutputStream(), true);
         mainServerReader = new BufferedReader(new InputStreamReader(mainServer.getInputStream()));
 
-        mainServerWriter.println("Message1");
+    }
+
+    public void chooseRoomName(String nameOfRoom) throws Exception{
+
+        mainServerWriter.println(nameOfRoom);
         String port = mainServerReader.readLine();
         mainServer.close();
-
-
+        chatServer = new Socket("192.168.0.73", Integer.parseInt(port));
     }
 
 }
--- ChatClient/src/ProgramStarter.java
+++ ChatClient/src/ProgramStarter.java
@@ -1,8 +1,19 @@
+import Connection.Client;
+import GUI.WelcomeWindow.*;
+
+
 public class ProgramStarter {
     public static void main(String[] args){
         Client client = new Client();
+
+
         try{
-            client.start();
+            client.initiateConnection();
+            WelcomeWindow.displayApplication(args, client);
+            System.out.println("aaa");
+
+
+
         }
         catch(Exception exception){
             exception.printStackTrace();
