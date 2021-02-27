31dd9c72bc0557a23ba2252d87ba3aed32150017
Maciej Mlaskawa <maciej.mlaskawa@gmail.com>
2021-02-27 12:55:59 +0100

    Implemented Basic Connection to server.

--- ChatClient/src/Client.java
+++ ChatClient/src/Client.java
@@ -1,2 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
+
+
 public class Client {
+    Socket mainServer;
+    PrintWriter mainServerWriter;
+    BufferedReader mainServerReader;
+
+    void start() throws Exception{
+
+        mainServer = new Socket("192.168.0.73", 2137);
+        mainServerWriter = new PrintWriter(mainServer.getOutputStream(), true);
+        mainServerReader = new BufferedReader(new InputStreamReader(mainServer.getInputStream()));
+
+        mainServerWriter.println("Message1");
+        String port = mainServerReader.readLine();
+        mainServer.close();
+
+
+    }
+
 }
--- ChatClient/src/ProgramStarter.java
+++ ChatClient/src/ProgramStarter.java
@@ -1,5 +1,11 @@
 public class ProgramStarter {
     public static void main(String[] args){
-
+        Client client = new Client();
+        try{
+            client.start();
+        }
+        catch(Exception exception){
+            exception.printStackTrace();
+        }
     }
 }
